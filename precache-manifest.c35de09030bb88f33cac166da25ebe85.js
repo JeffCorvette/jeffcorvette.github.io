13b1666b418567164e0d0a37d577625efa8c6745
DeFiDev <defidev2021@gmail.com>
2021-11-24 23:30:16 -0800

    feiwu cangshu

---
+++ precache-manifest.c35de09030bb88f33cac166da25ebe85.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0394eddd51551cf4d6dd",
+    "url": "/static/css/main.8c0a1448.chunk.css"
+  },
+  {
+    "revision": "0394eddd51551cf4d6dd",
+    "url": "/static/js/main.0394eddd.chunk.js"
+  },
+  {
+    "revision": "aca7b42372bd08e9824e",
+    "url": "/static/js/runtime~main.aca7b423.js"
+  },
+  {
+    "revision": "1b549924ab68b8616e7c",
+    "url": "/static/css/2.134d035e.chunk.css"
+  },
+  {
+    "revision": "1b549924ab68b8616e7c",
+    "url": "/static/js/2.1b549924.chunk.js"
+  },
+  {
+    "revision": "9598f5c2d3b1c004d1a5ed83f5a48ac5",
+    "url": "/static/media/iconFrame.9598f5c2.svg"
+  },
+  {
+    "revision": "59b679e5566b8c75d24cd02a6a238bfa",
+    "url": "/static/media/boxBgMobile.59b679e5.png"
+  },
+  {
+    "revision": "9e1629d0a0662e443f11ddba821a65be",
+    "url": "/static/media/modalBg.9e1629d0.png"
+  },
+  {
+    "revision": "6ec532ead63b2bd406ef04a3380f20c0",
+    "url": "/index.html"
+  }
+];
